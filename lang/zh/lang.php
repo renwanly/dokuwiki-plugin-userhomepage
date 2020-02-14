e279113e86fbee24f0244deaba6e9f62ef93ed2a
renwanly <renwanly@gmail.com>
2020-02-14 16:58:51 +0800

    Update lang.php

--- lang/zh/lang.php
+++ lang/zh/lang.php
@@ -1,14 +1,21 @@
 <?php
 
 /**
+ * Chinese language file for Userhomepage plugin
+ * @author renwanly <renwanly@163.com>
  * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
- *
- * @author Honvid <koran12@126.com>
  */
-$lang['copyerror']             = '复制错误';
-$lang['copysuccess']           = '复制成功';
-$lang['copynotneeded']         = '无需复制';
-$lang['createdprivatens']      = '创建私有命名空间';
-$lang['privatenamespace']      = '私有空间';
-$lang['publicpage']            = '公共页面';
-$lang['publicnamespace']       = '公共空间';
+$lang['copyerror'] = '复制错误';
+$lang['copysuccess'] = '复制成功';
+$lang['copynotneeded'] = '无需复制';
+$lang['createdprivatens'] = '创建私有命名空间';
+$lang['createdpublicpage'] = '创建您的公共页面';
+$lang['privatenamespace'] = '私有空间';
+$lang['publicpage'] = '公共页面';
+$lang['publicnamespace'] = '公共空间';
+$lang['uhpcreated'] = 'User Homepage插件自动创建';
+$lang['fromskeleton'] = '已将此页从User Homepage框架添加到您的私有命名空间：';
+$lang['aclupdate'] = '已添加或更新ACL规则：';
+$lang['mkdirfailure'] = '创建此目录失败：';
+$lang['settingsconflict'] = 'UserHomePage设置冲突！确保私有名称空间和公共名称空间不同。更正此问题前，插件会一直禁用。';
+$lang['avatarsconflict'] = 'UserHomePage设置与Avatar插件设置冲突！确保私有名称空间和Avatar存储在不同的位置（否则UserHomePage的ACL规则可能会阻止Avatar）。';
